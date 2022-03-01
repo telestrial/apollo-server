2d3b99540e47a7cffb2b85abc98ea4f9f59ac302
John W <telestrial@gmail.com>
2022-03-01 17:52:22 -0500

    Testing commits Trivial change to test commit process.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: (root, args, context) => "Hello world!"
+    hello: (root, args, context) => "Hello world"
   }
 };
 
